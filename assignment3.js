44fdaccd582ff717ff1ffe48596fe4919530406b
Anjali1814 <144798691+Anjali1814@users.noreply.github.com>
2025-06-26 11:31:28 +0530

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,28 @@
+let name = "Anjali";
+console.log(name.length);  // Output: 6
+
+
+let text = "hello";
+console.log(text.toUpperCase());  // Output: HELLO
+
+
+let texts = "WELCOME";
+console.log(text.toLowerCase());  // Output: welcome
+
+
+let str = "JavaScript";
+console.log(str.slice(0, 4));  // Output: "Java"
+console.log(str.slice(4));    // Output: "Script"
+ 
+
+let first = "Hello";
+let second = "World";
+console.log(first.concat(" ", second));  // Output: "Hello World"
+
+
+let sentence = "I love cats";
+console.log(sentence.replace("cats", "dogs"));  // Output: "I love dogs"
+
+
+let word = "JavaScript";
+console.log(word.charAt(4));  // Output: "S"
